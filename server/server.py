16402e75b4dcab0d9c09226d570584450af306ef
harshamin <hamin04@students.cs.ubc.ca>
2025-01-18 14:29:12 -0800

    login endpoint

--- server/server.py
+++ server/server.py
@@ -1,21 +1,31 @@
+import os
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+from flask_cors import CORS
+load_dotenv()
+
 
 # Create a Flask app instance
 app = Flask(__name__)
-
+CORS(app)
 # Define a route
 @app.route('/')
 def home():
     return "Flask server is running!"
 
-# Example API endpoint
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({
-        "message": "Hello from Flask!",
-        "status": "success"
-    })
+@app.route('/login', methods=['POST'])
+def handle_login():
+    data = request.get_json()
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"status": "failed", "message": "Missing credentials"})
+    
+    username = data['username']
+    password = data['password']
+    if username == os.getenv("USERNAME") and password == os.getenv("PASS"):
+        return jsonify({"status": "success"})
+    else:
+        return jsonify({"status": "failed", "message": "Invalid credentials"})
 
 # Run the server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)  
