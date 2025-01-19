3fc596522f96b618f438cb121d45f20ce416e1f2
Naman <nmiglani@students.cs.ubc.ca>
2025-01-18 16:26:54 -0800

    Created the Workout page with each workout to do

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import "./css/App.css";
 import LoginPage from "./Routes/LoginPage";
+import Workout from "./Routes/Workout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Workout />} />
       </Routes>
     </Router>
   );
