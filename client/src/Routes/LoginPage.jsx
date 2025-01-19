87fad1275fc32a5161a062bf1fb53590afbb165b
Anas Ahmed <anas31ahmed03@gmail.com>
2025-01-18 16:27:03 -0800

    created a homepag with previous workouts

--- client/src/Routes/LoginPage.jsx
+++ client/src/Routes/LoginPage.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import './LoginPage.css'; // Importing the CSS file
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/LoginPage.css"; // Importing the CSS file
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    const handleLogin = (event) => {
+        
+        navigate("homepage.jsx"); 
+    };
+
     return (
         <div className="login-container">
             <div className="login-box">
                 <h1 className="login-title">Login</h1>
-                <form className="login-form">
+                <form className="login-form" onSubmit={handleLogin}>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <input
                             type="email"
                             id="email"
                             name="email"
-                            placeholder="Enter your email"
+                            placeholder="Enter your username"
                             className="input-field"
                         />
                     </div>
@@ -27,11 +35,13 @@ const LoginPage = () => {
                             className="input-field"
                         />
                     </div>
-                    <button type="submit" className="login-button">Login</button>
+                    <button type="submit" className="login-button">
+                        Login
+                    </button>
                 </form>
             </div>
         </div>
     );
 };
 
-export default LoginPage; s
+export default LoginPage;
